6692d4dc34422d704ce1508a1179f8e42e94f5d7
veronbungag <rveron.bungag@gmail.com>
2024-01-24 10:14:48 +0800

    for loop

--- return_count_from_given_substring.py
+++ return_count_from_given_substring.py
@@ -1,6 +1,7 @@
 #define a function for counting emma
 def count_emma(statement):
 #for loop
+    for i in range(len(statement) - 1):
 #check if the substring of length 4 starting at the current index is equal to Emma
 #if loop is finished return count 
 #check the function and print the given statement
