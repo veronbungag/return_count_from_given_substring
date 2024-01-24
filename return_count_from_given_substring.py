608829473bb38798db59e07aebb5ed02874d9eb3
veronbungag <rveron.bungag@gmail.com>
2024-01-24 09:42:29 +0800

    pseudocode

---
+++ return_count_from_given_substring.py
@@ -0,0 +1,5 @@
+#define a function for counting emma
+#for loop
+#check if the substring of length 4 starting at the current index is equal to Emma
+#if loop is finished return count 
+#check the function and print the given statement
